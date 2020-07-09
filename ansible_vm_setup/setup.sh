2b837fd52ba36203854f9e5253ed39d77079bf19
henry brooks <brook057@cougars.csusm.edu>
2020-07-08 22:11:57 -0700

    Update setup.sh

--- ansible_vm_setup/setup.sh
+++ ansible_vm_setup/setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Set these to target ip address for the ansible host and your network gateway ip address
-GWADDR="192.168.0.1"
-IPADDR="192.168.0.99/24"
-ANSIBLEADDR="192.168.0.210/24"
+GWADDR="192.168.50.1"
+IPADDR="192.168.50.199/24"
+ANSIBLEADDR="192.168.50.210/24"
 ANSIBLEPASSWD="csusm"
 echo  "Update and Upgrade the Proxmox distribution"
 apt update -y
@@ -10,11 +10,11 @@ apt upgrade -y
 echo "Update pveam"
 #pveam update
 echo "Download LXC Ubuntu image"
-pveam download local ubuntu-18.04-standard_18.04.1-1_amd64.tar.gz
+pveam download local ubuntu-20.04-standard_20.04-1_amd64.tar.gz
 echo "Build LXC virtual machine"
-pct create 1000 /var/lib/vz/template/cache/ubuntu-18.04-standard_18.04.1-1_amd64.tar.gz -storage=local-lvm --hostname ubuntu1804 --password csusm
+pct create 1000 /var/lib/vz/template/cache/ubuntu-20.04-standard_20.04-1_amd64.tar.gz -storage=local-lvm --hostname ubuntu2004 --password csusm
 echo "Set network configuration for ubuntu vm"
-pct set 1000 -net0 name=eth0,bridge=vmbr0,ip=$IPADDR,gw=$GWADDR
+pct set 1000 -net0 name=eth0,bridge=vmbr0,firewall=1,ip=$IPADDR,gw=$GWADDR,type=veth
 echo "Start vm"
 pct start 1000
 echo "Wait for startup"
